981cb19d1590dffaba6a181c9d951dec4cad751c
caiomagalhaespires <98853712+caiomagalhaespires@users.noreply.github.com>
2022-03-03 20:03:34 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,113 @@
+var garden,rabbit;
+var gardenImg,rabbitImg;
+var grassImg
+var redImg
+var orangeImg
+var appleImg
+
+
+
+
+function preload(){
+  gardenImg = loadImage("garden.png");
+  rabbitImg = loadImage("rabbit.png");
+  appleImg = loadImage("apple.png");
+ orangeImg = loadImage("orangeLeaf.png");
+  redImg = loadImage("redImage.png"); 
+}
+
+function setup(){
+  
+  createCanvas(400,400);
+  
+
+garden=createSprite(200,200);
+garden.addImage(gardenImg);
+
+
+rabbit = createSprite(180,340,30,30);
+rabbit.scale =0.09;
+rabbit.addImage(rabbitImg);
+
+
+apple=createSprite(random(50,350),40,10,10);
+apple.addImage(appleImg);
+apple.scale=0.06;
+apple.velocityY= 4;
+apple.lifetime=150;
+
+
+redL=createSprite(random(50,350),40,10,10)
+redL.addImage(redImg);
+redL.scale=0.06;
+redL.velocityY= 4;
+redL.lifetime=150;
+
+
+orange=createSprite(random(50,350),40,10,10)
+orange.addImage(orangeImg);
+orange.scale=0.06;
+orange.velocityY= 4;
+orange.lifetime=150;
+
+
+
+
+
+
+
+
+}
+
+
+function draw() {
+  background(0);
+
+
+  edges= createEdgeSprites();
+  rabbit.collide(edges);
+
+  drawSprites();
+  
+
+  rabbit.x = World.mouseX;
+  
+  
+  var select_sprites = Math.round(random(1,3));
+  
+  if (frameCount % 80 == 0) {
+    if (select_sprites == 1) {
+      createApples();
+    } else if (select_sprites == 2) {
+      createOrange();
+    }else {
+      createRed();
+    }
+  }
+
+}
+
+function createApples() {
+apple = createSprite(random(50, 350),40, 10, 10);
+apple.addImage(appleImg);
+apple.scale=0.07;
+apple.velocityY = 3;
+apple.lifetime = 150;
+  
+}
+
+function createOrange() {
+orangeL = createSprite(random(50, 350),40, 10, 10);
+orangeL.addImage(orangeImg);
+orangeL.scale=0.08;
+orangeL.velocityY = 3;
+orangeL.lifetime = 150;
+}
+
+function createRed() {
+redL = createSprite(random(50, 350),40, 10, 10);
+redL.addImage(redImg);
+redL.scale=0.06;
+  redL.velocityY = 3;
+  redL.lifetime = 150;
+}
